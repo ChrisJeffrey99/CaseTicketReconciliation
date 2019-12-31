24dd7a88604df5a8f3950d803aad11c7dd938b72
Chris Jeffrey <Chris.Jeffrey@encorebusiness.com>
2019-12-31 17:26:09 -0600

    Got CSV Reader working in Program.cs Added SQL Database Install file

--- CaseTicketReconciliation/CaseTicketReconciliation/Program.cs
+++ CaseTicketReconciliation/CaseTicketReconciliation/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,23 @@ namespace CaseTicketReconciliation
     {
         static void Main(string[] args)
         {
+            const char delimeter = ',';
+            List<string[]> cells = new List<string[]>();
+
+            StreamReader sr = new StreamReader(File.OpenRead("C:/Users/CJeffrey/OneDrive - Encore Business Solutions Inc/Documents/Care/Case Reconciliation/Reconcile/Freshdesk_Care-OpenTickets.csv"));
+
+            int i = 0;
+            while (!sr.EndOfStream)
+            {
+                cells.Add(sr.ReadLine().Split(delimeter));
+
+                Console.Write("Row: " + i + " | ");
+                for (int x = 0; x < cells[i].Length; x++)
+                {
+                    Console.Write(x + " || " + "Cell Contents: " + cells[i][x].ToString());
+                }
+                i++;
+            }
         }
     }
 }
---
+++ CaseTicketReconciliation/CaseTicketReconciliation/install.sql
@@ -0,0 +1,53 @@
+﻿--Init Database
+-------------------------------------------------------
+use master
+go
+drop database CaseTicketReconcile
+go
+create database CaseTicketReconcile
+go
+use CaseTicketReconcile
+go
+
+--CREATE TABLES
+-------------------------------------------------------
+
+CREATE TABLE tbFreshDesk
+(
+ID INT PRIMARY KEY NOT NULL,
+TicketStatus varchar(50) NOT NULL,
+CaseNumber VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE tbCRM
+(
+ID varchar(50) PRIMARY KEY NOT NULL,
+CaseStatus varchar(50) NOT NULL,
+TicketNumber VARCHAR(50) NOT NULL
+)
+
+go
+--CREATE STORED PROCEDURES
+-------------------------------------------------------
+
+CREATE PROCEDURE spAddFreshDeskRow @ID INT, @TicketStatus VARCHAR(50), @CaseNumber VARCHAR(50)
+as
+	INSERT INTO tbFreshDesk (ID,TicketStatus,CaseNumber) VALUES
+							(@ID, @TicketStatus, @CaseNumber)
+go
+
+CREATE PROCEDURE spAddCRMRow @ID VARCHAR(50), @CaseStatus VARCHAR(50), @TicketNumber INT
+as
+	INSERT INTO tbCRM (ID,CaseStatus,TicketNumber) VALUES
+							(@ID, @CaseStatus, @TicketNumber)
+go
+
+
+exec spAddFreshDeskRow 5,'closed','CAS-TEST';
+exec spAddCRMRow 'CAS-TEST','Resolved',676;
+
+SELECT *
+FROM tbCRM
+
+SELECT *
+FROM tbFreshDesk;
